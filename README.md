b9b8ed8e9b95d38afe99be2baf3c17c60fbf0664
Pankaj Yadav <panky.asg@gmail.com>
2021-02-15 17:58:28 +0530

    Added: docker-compose, restapi code comments

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-dockerised-flask
+#dockerised-flask
 
-docker build -t sum_api .
+##Restful API for Conditional sum of 3 input integers.
+
+Flask micro web-framework and docker.
+
+##Steps to build project
+
+docker-compose build
+
+docker-compose up -d 
--- rest.py
+++ rest.py
@@ -5,31 +5,38 @@ import functools
 
 app = Flask(__name__)
 
+"""
+flask route handler function for /sum
+"""
 @app.route("/sum",  methods = ['POST'])
 def post():
     req_body = request.get_json()
     if 'data' in req_body:
         lis = req_body['data']
 
-        # print("lis", lis)
+        # Input Integer count check
         if len(lis) < 3 or len(lis) > 3:
             return json.dumps({'status': 400, 'error': 'Exactly 3 numbers are required'}), 400, {'ContentType':'application/json'} 
 
         a, b, c = lis[:]
 
+        # Allow only integers
         for x in lis:
             if type(x) != int:
                 return json.dumps({'status': 400, 'error': 'All inputs must be numeric'}), 400, {'ContentType':'application/json'} 
 
+        # Typecast string to integer
         lis = [int(n) for n  in lis]
 
+        # Input integers acceptable value filtering
         lis2 = [0 if 13 <= x <= 19 and x not in [15, 16] else x for x in lis]
 
+        # Add filtered list integers
         s = functools.reduce(lambda x, y: x + y, lis2)
         return json.dumps({'status': 200, 'result': s}), 200, {'ContentType':'application/json'} 
 
     else:
-        return json.dumps({'status': 400, 'error': 'Something went wrong'}), 400, {'ContentType':'application/json'} 
+        return json.dumps({'status': 400, 'error': 'Invalid input'}), 400, {'ContentType':'application/json'} 
 
 
 if __name__ == "__main__":
